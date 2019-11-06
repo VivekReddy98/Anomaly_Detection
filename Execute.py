98b99341a60097f8753aacc4d8a42b7aef317c60
Vivek Reddy Karri <vivekreddykarri98@gmail.com>
2019-11-05 19:28:03 -0500

    Final Results documented

--- Execute.py
+++ Execute.py
@@ -5,7 +5,19 @@ from scipy import sparse
 import numpy as np
 import os, json
 from copy import deepcopy
-import sys, ast
+import sys, ast, time
+
+def Moving_Range_Mean(lis):
+    l = lis.shape[0]
+    arr = lis - np.roll(lis, -1)
+    return np.nansum(np.absolute(arr[1:-1]))/(l-1)
+
+def give_stats(lis):
+    mean_ = Moving_Range_Mean(lis)
+    median_ = np.nanmedian(lis)
+    upper_t = median_ + 3*mean_
+    lower_t = median_ - 3*mean_
+    return mean_, median_, lower_t, upper_t
 
 def Parse(list_files):
 	new = {}
@@ -24,8 +36,8 @@ def GiveAffinity(EdgeFilePath, g, epsilon):
 	return D.ComputeAffinityMatrix(I1, D1, A1, S1)
 
 def Similarity(AM1, AM2):
-	AM1.data = np.sqrt(AM1.data)
-	AM2.data = np.sqrt(AM2.data)
+	AM1.data = np.sqrt(AM1.data.clip(0))
+	AM2.data = np.sqrt(AM2.data.clip(0))
 	Result = AM1-AM2
 	d = np.sqrt(np.sum(np.square(Result.data)))
 	return (1/(1+d))
@@ -34,18 +46,40 @@ def compute_similarity(list_files, dict_files, dataset, g, epsilon, results_dir)
 	first_computed = False
 	with open(results_dir+dataset+"/"+"time_series"+"_"+str(g)+"_"+str(round(epsilon,2))+".txt", "w+") as f:
 		for i in range(0,len(list_files)-1):
+			start = time.time()
 			if not(first_computed):
 				AM_old = GiveAffinity(EdgeFilePath="datasets/{}/{}".format(dataset, dict_files[list_files[i]]), g=g, epsilon=epsilon)
 				first_computed = True
 			else:
 				AM_old = AM_new
 			AM_new = GiveAffinity(EdgeFilePath="datasets/{}/{}".format(dataset, dict_files[list_files[i+1]]), g=g, epsilon=epsilon)
+			print("Afinity Matrices computed, about to find Similarity")
 			sim_score = Similarity(AM_old, deepcopy(AM_new))
 			f.write(str(sim_score))
 			f.write("\n")
-			print(dict_files[list_files[i]], dict_files[list_files[i+1]], round(sim_score,5))
+			print("Time taken for computing sim score between {} and {} is {} secs".format(dict_files[list_files[i]], dict_files[list_files[i+1]], (time.time()-start)))
 	print("The scores have been documented")
 	return None 
+
+def find_anomalies(results_dir, dataset, param[0], param[1]):
+	path = results_dir+dataset+"/"+"time_series"+"_"+str(g)+"_"+str(round(epsilon,2))+".txt"
+	time_series = np.loadtxt(path)
+	mean_, median_, lower_t, upper_t = give_stats(time_series)
+	anomalous_similarities = []
+	with open(results_dir+dataset+"/"+"anomalous_similarities"+"_"+str(g)+"_"+str(round(epsilon,2))+".txt", "w+") as f:
+		for i in range(0, time_series.shape[0]):
+			if time_series[i] < lower_t:
+				anomalous_similarities.append(i)
+				f.write(str(anomalous_similarities))
+				f.write("\n")
+	with open(results_dir+dataset+"/"+"anomalous_time_stamp"+"_"+str(g)+"_"+str(round(epsilon,2))+".txt", "w+") as f:
+		for i in range(1, len(anomalous_similarities)):
+			if anomalous_similarities[i]-anomalous_similarities[i-1] == 1:
+				f.write(str(anomalous_similarities[i]))
+				f.write("\n")
+	print("Anomalies written to the results folder ")
+	return None
+
 
 ## Testing this class
 if __name__ == '__main__':	
@@ -61,5 +95,6 @@ if __name__ == '__main__':
 		list_files = list(dict_files.keys())
 		list_files.sort()
 		compute_similarity(list_files, dict_files, dataset, param[0], param[1], results_dir)
+		find_anomalies(results_dir, dataset, param[0], param[1])
 
 	
--- Utils/DeltaCon.py
+++ Utils/DeltaCon.py
@@ -2,11 +2,8 @@ import scipy, math
 from scipy import sparse
 import numpy as np
 import networkx as nx
-from sklearn.metrics.pairwise import pairwise_distances 
-from random import shuffle
 from scipy.spatial.distance import cdist
 from scipy.sparse.linalg import spsolve
-import time
 from Utils.GenerateGraphMatrices import GenerateGraphMatrices
 
 class DeltaCon():
@@ -18,37 +15,37 @@ class DeltaCon():
 		A.data = A.data*self.epsilon
 		Left_operand = I + D - A
 		AffinityMatrix = spsolve(Left_operand, S)
-		print(np.sum(AffinityMatrix.data), np.sum(S.data))
+		print("Affinity Matrix Computed :)")
 		return AffinityMatrix
 
-def Similarity(AM1, AM2):
-	AM1.data = np.sqrt(AM1.data)
-	AM2.data = np.sqrt(AM2.data)
-	Result = AM1-AM2
-	d = np.sqrt(np.sum(np.square(Result.data)))
-	print(d)
-	return (1/(1+d))
+# def Similarity(AM1, AM2):
+# 	AM1.data = np.sqrt(AM1.data)
+# 	AM2.data = np.sqrt(AM2.data)
+# 	Result = AM1-AM2
+# 	d = np.sqrt(np.sum(np.square(Result.data)))
+# 	print(d)
+# 	return (1/(1+d))
 
 
-## Testing this class
-if __name__ == '__main__':
-	Delta = DeltaCon(0.9)
+# ## Testing this class
+# if __name__ == '__main__':
+# 	Delta = DeltaCon(0.9)
 	
-	start = time.time()
-	G = GenerateGraphMatrices(500)
-	I1, S1, A1, D1 = G.GenerateGraphObj(EdgeFilePath="datasets/autonomous/0_autonomous.txt")
-	AM1 = Delta.ComputeAffinityMatrix(I1, D1, A1, S1)
-	print((time.time()-start))
+# 	start = time.time()
+# 	G = GenerateGraphMatrices(500)
+# 	I1, S1, A1, D1 = G.GenerateGraphObj(EdgeFilePath="datasets/autonomous/0_autonomous.txt")
+# 	AM1 = Delta.ComputeAffinityMatrix(I1, D1, A1, S1)
+# 	print((time.time()-start))
 
-	start = time.time()
-	G = GenerateGraphMatrices(500)
-	I2, S2, A2, D2 = G.GenerateGraphObj(EdgeFilePath="datasets/autonomous/1_autonomous.txt")
-	AM2 = Delta.ComputeAffinityMatrix(I2, D2, A2, S2)
-	print((time.time()-start))
+# 	start = time.time()
+# 	G = GenerateGraphMatrices(500)
+# 	I2, S2, A2, D2 = G.GenerateGraphObj(EdgeFilePath="datasets/autonomous/1_autonomous.txt")
+# 	AM2 = Delta.ComputeAffinityMatrix(I2, D2, A2, S2)
+# 	print((time.time()-start))
 
-	#print(np.sum(A1), np.sum(A2), np.sum(D1), np.sum(D2), np.sum(AM1), np.sum(AM2))
+# 	#print(np.sum(A1), np.sum(A2), np.sum(D1), np.sum(D2), np.sum(AM1), np.sum(AM2))
 
-	print(Similarity(AM1,AM2))
+# 	print(Similarity(AM1,AM2))
 
 
 
