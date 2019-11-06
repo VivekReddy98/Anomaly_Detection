da2bf6f6421438e846ffb7da0b515fa6ec2c1aa2
Vivek Reddy Karri <vivekreddykarri98@gmail.com>
2019-11-05 21:25:25 -0500

    Files have been outputted under the name specified

--- automate.sh
+++
@@ -1,8 +0,0 @@
-echo "Please enter your choice of dataset"
-read dataset
-
-export start=$SECONDS
-python Execute.py $dataset >> logs/$dataset.log
-
-elapsedtime=$(($SECONDS-$start))
-echo It took $elapsedtime seconds to complete the dataset $dataset on the complete search space provided in the json file >> time_taken.txt
